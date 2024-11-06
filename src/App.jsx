42b8f2e62daa3d9eab1ac11bba37117260e6e28f
sparrow <makcvoin09@gmail.com>
2024-11-06 03:00:16 +0200

    f

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1 className=''>Виконана робота по ІТ-праву</h1>
-      <button type='button'style={{border:"black solid 1px"}} ><a href="../conspect.html" style={{color: "green"}} >конспект</a></button>
+      <button type='button'style={{border:"black solid 1px"}} ><a href="./conspect.html" style={{color: "green"}} >конспект</a></button>
       <SudList/>
     </>
   )
